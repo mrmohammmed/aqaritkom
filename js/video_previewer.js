efd52c2fb2329d477044626ae3363aef46cb3775
mohamed ahmed <mohamed@gmail.com>
2024-02-22 10:27:56 +0200

    edits

--- js/video_previewer.js
+++
@@ -1,9 +0,0 @@
-const inputFile = document.getElementById("video_uploader");
-const video = document.getElementById("video");
-
-inputFile.addEventListener("change", function () {
-    const file = inputFile.files[0];
-    const videourl = URL.createObjectURL(file);
-    video.setAttribute("src", videourl);
-    video.play();
-});
